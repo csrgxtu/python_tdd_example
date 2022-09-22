514cb88acee51807bb8ecd66e61053292ec9e740
muyakongali <muyakongali@tencent.com>
2022-09-22 16:04:21 +0800

    functional tests tells a story

--- functional_tests.py
+++ functional_tests.py
@@ -5,6 +5,31 @@ from selenium import webdriver
 # in mac make chromedriver runnable by setting perm: xattr -d com.apple.quarantine path/chromedriver
 # install geckodriver: brew install geckodriver
 browser = webdriver.Chrome()
+
+# user has heard about a cool new online to-do app, she goes to checkout its homepage
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# she notices the page title and header mention to-do lists
+assert 'To-Do' in browser.title
+
+# she is invited to enter a to-do item straight away
+
+# she types "Buy peacock feathers" into a text box
+
+# when she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# there is still a text box inviting her to add another item. she
+# enters "Use peaco0ck feathers to make a fly"
+
+# the page updates again, and now shows both items on her list
+
+# she wonders whether the site will remember her list. then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect
+
+# she visists that url - here to-do list is still there
+
+# statisfied, she goes back to sleep
+
+browser.quit()
